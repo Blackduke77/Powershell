63044227f1db20d705dfe0b1ac2f172111be4c82
Blackduke77 <blackduke77@hotmail.com>
2014-08-17 02:21:27 +0100

    working script

--- Spaces-Create_Cluster_CSVs.ps1
+++ Spaces-Create_Cluster_CSVs.ps1
@@ -1,8 +1,25 @@
 ﻿## Create CSV's
 ## Sources that helped
 ## http://www.aidanfinn.com/?p=15239
-##
+## http://superwidgets.wordpress.com/2014/06/22/using-powershell-with-tiered-mirrored-storage-spaces/
+#
+#
+$TieredMirroredvDisks = @("SMB1","SMB2","SMB3","SMB4","SMB5","SMB6")
 
+$Loc = Get-Location
+$Date = Get-Date -format yyyyMMdd_hhmmsstt
+$logfile = $Loc.path + “\CreateSS_” + $Date + “.txt”
+#
+function log($string, $color)
+{
+ if ($Color -eq $null) {$color = “white”}
+ write-host $string -foregroundcolor $color 
+ $temp = “: ” + $string
+ $string = Get-Date -format “yyyy.MM.dd hh:mm:ss tt”
+ $string += $temp 
+ $string | out-file -Filepath $logfile -append
+}
+#
 Function PrepCSV ($CSVName)
  {
  #Rename the disk resource in FCM
@@ -33,7 +50,21 @@ Function PrepCSV ($CSVName)
  Get-ClusterGroup "Available Storage" | Move-ClusterGroup -Node $MyName
 
  #$Storagepool = Get-StoragePool | ? {$_.FriendlyName -match $PoolName}
+ ForEach ($vDisk in $TieredMirroredvDisks) 
+ {
+ log “Attempting to create vDisk ‘$vDisk’..”
  $Pools = Get-StoragePool | ? {$_.FriendlyName -like "*pool*"}
  $WPool = $Pools[0].FriendlyName
- Get-StoragePool $WPool | New-VirtualDisk -FriendlyName $WPool-SMB02 -IsEnclosureAware $True -ResiliencySettingName Mirror -NumberOfDataCopies 3 -Interleave 65536 –StorageTiers $ssd_tier, $hdd_tier -StorageTierSizes 264GB, 20480GB -WriteCacheSize 5GB -ProvisioningType Fixed
- PrepCSV $WPool-SMB02
+ <#
+ Get-StoragePool $WPool | New-VirtualDisk -FriendlyName $WPool-SMB03 -IsEnclosureAware $True -ResiliencySettingName Mirror -NumberOfDataCopies 3 -Interleave 65536 –StorageTiers $ssd_tier, $hdd_tier -StorageTierSizes 264GB, 20480GB -WriteCacheSize 5GB -ProvisioningType Fixed
+
+ PrepCSV $WPool-SMB03
+ #>
+
+ $Status = Get-StoragePool $WPool | New-VirtualDisk -FriendlyName $WPool-$vDisk -IsEnclosureAware $True -ResiliencySettingName Mirror -NumberOfDataCopies 3 -NumberOfColumns 4 -Interleave 65536 –StorageTiers $ssd_tier, $hdd_tier -StorageTierSizes 264GB, 5000GB -WriteCacheSize 5GB -ProvisioningType Fixed
+ log ($Status | Out-String)
+ if ($Status.OperationalStatus -eq “OK”){ 
+ log “vDisk ‘$vDisk’ creation succeeded” green}
+ else { log “vDisk ‘$vDisk’ creation failed..stopping” yellow; break }
+ PrepCSV $WPool-$vDisk
+ }
