f6482248c6ce1898360d496ef107d6567c81e175
Blackduke77 <robbie.roberts@outlook.com>
2014-08-25 17:04:19 +0100

    added some execution parameters
    updated a few things and added some execution parameters

--- Create-Pool.ps1
+++ Create-Pool.ps1
@@ -1,19 +1,42 @@
-## CREATE AND CONFIGURE STORAGE POOLS AND SPACES ##
-## Author Robbie Roberts
-## 15 August 2014
+<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2014 v4.1.63
+	 Created on:   	25/08/2014 16:20
+	 Created by:   	Robbie roberts
+	 Organization: 	
+	 Filename:     	Create-Pool.ps1
+	===========================================================================
+	.DESCRIPTION
+		This script creates a Storage Space Disk Pool using all Physical disks that show they can pool.
+		this script is wrtten for use on a scale out file server using clustered storage, it does not
+		work with standalone storage.
 
+	.EXAMPLE
+		.\Create-Pool.ps1 -PoolName DiskPool1
 
-##Create the Storage Pool
+#>
+
+param ([string]$PoolName)
+
+Write-Host "Creating Storage Pool" -ForegroundColor 'Green'
+
+if (!($PoolName))
+{
+	Write-Host "Please enter a pool name" -ForegroundColor 'Red'
+	Write-Host "Usage: .\Create-Pool.ps1 -PoolName <>" -ForegroundColor 'Yellow'
+	Exit 0
+}
+
+# Create the Storage Pool
 
 # Get the FriendlyName of the Storage Sub System and pass into variable
 # Get all Physical Disks that can be pooled and pass into variable
 
 $pool01subsystem = Get-StorageSubSystem | ? FriendlyName -like "*Clustered*"
 $PhysicalDisks = (Get-PhysicalDisk -CanPool $True)
-$Pool1Name = "AER-Pool1"
 
-New-StoragePool -StorageSubSystemId $pool01subsystem.UniqueId -FriendlyName $Pool1Name -PhysicalDisks $PhysicalDisks
-
+New-StoragePool -StorageSubSystemId $pool01subsystem.UniqueId -FriendlyName $PoolName -PhysicalDisks $PhysicalDisks
 
 <#
 ## Remove Storage Pool
